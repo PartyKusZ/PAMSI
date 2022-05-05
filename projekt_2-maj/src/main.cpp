417b3927c250a3db056124e0939f7676d1942fcd
Jakub Kusz <jakub.kusz@onet.pl>
2022-05-06 00:37:42 +0200

    gotowe

--- projekt_2-maj/src/main.cpp
+++ projekt_2-maj/src/main.cpp
@@ -45,29 +45,64 @@ int main(){
     for(int i = 0; i < SIZE_MAXIMUM; ++i){
         tabmax[i] = movie_rating_list[i].rating;
     }
-    //test_qsort_10000(tab10000);
-    //test_qsort_100000(tab100000);
-    //test_qsort_500000(tab500000);
-    //test_qsort_962903(tabmax);
-    //test_merge_sort_10000(tab10000);
-    //test_merge_sort_100000(tab100000);
-    //test_merge_sort_500000(tab500000);
-    //test_merge_sort_962903(tabmax);
-    //test_bucket_sort_10000(tab10000);
-    //test_bucket_sort_100000(tab100000);
-    //test_bucket_sort_500000(tab500000);
-    //test_bucket_sort_962903(tabmax);
-    test_intro_sort_10000(tab10000);
-    test_intro_sort_100000(tab100000);
-    test_intro_sort_500000(tab500000);
-    test_intro_sort_962903(tabmax);
+    //  test_quick_sort_10000(tab10000);
+    //  test_quick_sort_100000(tab100000);
+    //  test_quick_sort_500000(tab500000);
+    //  test_quick_sort_962903(tabmax);
+    // test_merge_sort_10000(tab10000);
+    // test_merge_sort_100000(tab100000);
+    // test_merge_sort_500000(tab500000);
+    // test_merge_sort_962903(tabmax);
+  //test_intro_sort_10000(tab10000);
+  //test_intro_sort_100000(tab100000);
+  //test_intro_sort_500000(tab500000);
+  //test_intro_sort_962903(tabmax);
 
     // bucket_sort(tab500000,20);
     // for (int i = 0; i < 20; i++)
     // {
     //     std :: cout << tab500000[i] << '\n';
     // }
+     double art10000;
+     double art100000;
+     double art500000;
+     double artmax;
+   merge_sort(tab10000,0,  SIZE_10000);
+   std :: cout << "Done" << '\n';
+   merge_sort(tab100000,0,  SIZE_100000);
+   std :: cout << "Done" << '\n';
+    merge_sort(tab500000,0, SIZE_500000);
+   std :: cout << "Done" << '\n';
+    merge_sort(tabmax, 0, SIZE_MAXIMUM);
+   std :: cout << "Done" << '\n';
+
+
+
+    art10000 = (std :: accumulate(tab10000,tab10000 + SIZE_10000,0) /static_cast<double>(SIZE_10000));
+    art100000 = std :: accumulate(tab100000,tab100000 + SIZE_100000,0) / static_cast<double>(SIZE_100000);
+    art500000 = std :: accumulate(tab500000,tab500000 + SIZE_500000,0) / static_cast<double>(SIZE_500000);
+    artmax = std :: accumulate(tabmax,tabmax + SIZE_MAXIMUM,0) / static_cast<double>(SIZE_MAXIMUM);
+
     
+
+    std :: cout << art10000 << '\n';
+    std :: cout << art100000 << '\n';
+    std :: cout << art500000 << '\n';
+    std :: cout << artmax << '\n';
+    std :: cout <<  '\n';
+
+    std :: cout << tab10000[SIZE_10000 / 2] << '\n';
+    std :: cout << tab100000[SIZE_100000 / 2] << '\n';
+    std :: cout << tab500000[SIZE_500000 / 2] << '\n';
+    std :: cout << (tabmax[SIZE_MAXIMUM / 2] + tabmax[(SIZE_MAXIMUM / 2) + 1] ) / 2.0  << '\n';
+
+
+
+
+
+
+
+
 
    
     
